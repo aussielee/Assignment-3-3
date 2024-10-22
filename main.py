fc454a765a21c18b467d70f3fe0979e945b89eac
aussielee <50060992+aussielee@users.noreply.github.com>
2024-10-21 20:21:59 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,35 @@
+# Function:     This program determines if a date entered by the user is valid.  
+# Input:        Interactive
+# Output:       Valid date is printed or user is alerted that an invalid date was entered.
+
+validDate = True
+MIN_YEAR = 0
+MIN_MONTH = 1
+MAX_MONTH = 12
+MIN_DAY = 1
+MAX_DAY = 31
+
+year = None
+month = None
+day = None
+
+# Get the year, then the month, then the day
+year = input("Enter year: ")  # Prompt user to enter the year
+month = input("Enter month: ")  # Prompt user to enter the month
+day = input("Enter day: ")  # Prompt user to enter the day
+
+# Check to be sure date is valid
+if int(year) <= MIN_YEAR:  # invalid year
+    validDate = False
+elif int(month) < MIN_MONTH or int(month) > MAX_MONTH:  # invalid month
+    validDate = False
+elif int(day) < MIN_DAY or int(day) > MAX_DAY:  # invalid day
+    validDate = False
+
+# Test to see if date is valid and output date and whether it is valid or not
+if validDate == True:
+    print(f"{month}/{day}/{year} is a valid date.")  # Output valid date statement
+else:
+    print(f"{month}/{day}/{year} is an invalid date.")  # Output invalid date statement
+
+# endOfJob()
